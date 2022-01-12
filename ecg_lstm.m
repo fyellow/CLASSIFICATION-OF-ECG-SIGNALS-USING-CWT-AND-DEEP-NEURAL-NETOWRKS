53096a4681ae556fe7008b0aebcaa37a901b8b1a
fyellow <59092391+fyellow@users.noreply.github.com>
2022-01-12 10:27:04 +0530

    Update ecg_lstm.m

--- ecg_lstm.m
+++ ecg_lstm.m
@@ -1,5 +1,165 @@
-load('ECGData.mat')
-data=ECGData.Data;
-Labels=ECGData.Labels;
-signallength=500;
+if ~isfile('PhysionetData.mat')
+    ReadPhysionetData         
+end
+load PhysionetData
+L = cellfun(@length,Signals);
+h = histogram(L);
+xticks(0:3000:18000);
+xticklabels(0:3000:18000);
+title('Signal Lengths')
+xlabel('Length')
+ylabel('Count')
+normal = Signals{1};
+aFib = Signals{4};
+
+subplot(2,1,1)
+plot(normal)
+title('Normal Rhythm')
+xlim([4000,5200])
+ylabel('Amplitude (mV)')
+text(4330,150,'P','HorizontalAlignment','center')
+text(4370,850,'QRS','HorizontalAlignment','center')
+
+subplot(2,1,2)
+plot(aFib)
+title('Atrial Fibrillation')
+xlim([4000,5200])
+xlabel('Samples')
+ylabel('Amplitude (mV)')
 [Signals,Labels] = segmentSignals(Signals,Labels);
+afibX = Signals(Labels=='A');
+afibY = Labels(Labels=='A');
+
+normalX = Signals(Labels=='N');
+normalY = Labels(Labels=='N');
+[trainIndA,~,testIndA] = dividerand(718,0.9,0.0,0.1);
+[trainIndN,~,testIndN] = dividerand(4937,0.9,0.0,0.1);
+
+XTrainA = afibX(trainIndA);
+YTrainA = afibY(trainIndA);
+
+XTrainN = normalX(trainIndN);
+YTrainN = normalY(trainIndN);
+
+XTestA = afibX(testIndA);
+YTestA = afibY(testIndA);
+
+XTestN = normalX(testIndN);
+YTestN = normalY(testIndN);
+XTrain = [repmat(XTrainA(1:634),7,1); XTrainN(1:4438)];
+YTrain = [repmat(YTrainA(1:634),7,1); YTrainN(1:4438)];
+
+XTest = [repmat(XTestA(1:70),7,1); XTestN(1:490)];
+YTest = [repmat(YTestA(1:70),7,1); YTestN(1:490);];
+layers = [ ...
+    sequenceInputLayer(1)
+    bilstmLayer(100,'OutputMode','last')
+    fullyConnectedLayer(2)
+    softmaxLayer
+    classificationLayer
+    ]
+ options = trainingOptions('adam', ...
+    'MaxEpochs',10, ...
+    'MiniBatchSize', 150, ...
+    'InitialLearnRate', 0.01, ...
+    'SequenceLength', 1000, ...
+    'GradientThreshold', 1, ...
+    'ExecutionEnvironment',"auto",...
+    'plots','training-progress', ...
+    'Verbose',false);
+net = trainNetwork(XTrain,YTrain,layers,options);
+trainPred = classify(net,XTrain,'SequenceLength',1000);
+LSTMAccuracy = sum(trainPred == YTrain)/numel(YTrain)*100
+figure
+confusionchart(YTrain,trainPred,'ColumnSummary','column-normalized',...
+              'RowSummary','row-normalized','Title','Confusion Chart for LSTM');
+fs = 300;
+
+figure
+subplot(2,1,1);
+pspectrum(normal,fs,'spectrogram','TimeResolution',0.5)
+title('Normal Signal')
+
+subplot(2,1,2);
+pspectrum(aFib,fs,'spectrogram','TimeResolution',0.5)
+title('AFib Signal')
+[instFreqA,tA] = instfreq(aFib,fs);
+[instFreqN,tN] = instfreq(normal,fs);
+
+figure
+subplot(2,1,1);
+plot(tN,instFreqN)
+title('Normal Signal')
+xlabel('Time (s)')
+ylabel('Instantaneous Frequency')
+
+subplot(2,1,2);
+plot(tA,instFreqA)
+title('AFib Signal')
+xlabel('Time (s)')
+ylabel('Instantaneous Frequency')
+instfreqTrain = cellfun(@(x)instfreq(x,fs)',XTrain,'UniformOutput',false);
+instfreqTest = cellfun(@(x)instfreq(x,fs)',XTest,'UniformOutput',false);
+[pentropyA,tA2] = pentropy(aFib,fs);
+[pentropyN,tN2] = pentropy(normal,fs);
+
+figure
+
+subplot(2,1,1)
+plot(tN2,pentropyN)
+title('Normal Signal')
+ylabel('Spectral Entropy')
+
+subplot(2,1,2)
+plot(tA2,pentropyA)
+title('AFib Signal')
+xlabel('Time (s)')
+ylabel('Spectral Entropy')
+pentropyTrain = cellfun(@(x)pentropy(x,fs)',XTrain,'UniformOutput',false);
+pentropyTest = cellfun(@(x)pentropy(x,fs)',XTest,'UniformOutput',false);
+XTrain2 = cellfun(@(x,y)[x;y],instfreqTrain,pentropyTrain,'UniformOutput',false);
+XTest2 = cellfun(@(x,y)[x;y],instfreqTest,pentropyTest,'UniformOutput',false);
+mean(instFreqN)
+mean(pentropyN)
+XV = [XTrain2{:}];
+mu = mean(XV,2);
+sg = std(XV,[],2);
+
+XTrainSD = XTrain2;
+XTrainSD = cellfun(@(x)(x-mu)./sg,XTrainSD,'UniformOutput',false);
+
+XTestSD = XTest2;
+XTestSD = cellfun(@(x)(x-mu)./sg,XTestSD,'UniformOutput',false);
+instFreqNSD = XTrainSD{1}(1,:);
+pentropyNSD = XTrainSD{1}(2,:);
+
+mean(instFreqNSD)
+mean(pentropyNSD)
+layers = [ ...
+    sequenceInputLayer(2)
+    bilstmLayer(100,'OutputMode','last')
+    fullyConnectedLayer(2)
+    softmaxLayer
+    classificationLayer
+    ]
+options = trainingOptions('adam', ...
+    'MaxEpochs',30, ...
+    'MiniBatchSize', 150, ...
+    'InitialLearnRate', 0.01, ...
+    'GradientThreshold', 1, ...
+    'ExecutionEnvironment',"auto",...
+    'plots','training-progress', ...
+    'Verbose',false);
+net2 = trainNetwork(XTrainSD,YTrain,layers,options);
+trainPred2 = classify(net2,XTrainSD);
+LSTMAccuracy = sum(trainPred2 == YTrain)/numel(YTrain)*100
+figure
+confusionchart(YTrain,trainPred2,'ColumnSummary','column-normalized',...
+              'RowSummary','row-normalized','Title','Confusion Chart for LSTM');
+testPred2 = classify(net2,XTestSD);
+
+LSTMAccuracy = sum(testPred2 == YTest)/numel(YTest)*100
+figure
+confusionchart(YTest,testPred2,'ColumnSummary','column-normalized',...
+              'RowSummary','row-normalized','Title','Confusion Chart for LSTM');
+
